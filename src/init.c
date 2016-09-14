60400706b0d945feb7e621e6df9d36663ecdc0ae
vikas84bf <vikas.ramachandra@gmail.com>
2016-09-14 16:04:14 -0700

    add new c functions to init.c, and R functions to namespace

--- src/init.c
+++ src/init.c
@@ -8,13 +8,18 @@ SEXP rpartexp2(SEXP dtimes, SEXP seps);
 SEXP pred_rpart(SEXP dimx, SEXP nnode, SEXP nsplit, SEXP dimc,
 		SEXP nnum, SEXP nodes2, SEXP vnum, SEXP split2,
 		SEXP csplit2, SEXP usesur, SEXP xdata2, SEXP xmiss2);
-
+SEXP honest_estimate_rpart(SEXP dimx, SEXP nnode, SEXP nsplit, SEXP dimc,
+                        SEXP nnum, SEXP nodes2, SEXP n1, SEXP wt1, SEXP dev1, SEXP yval1,
+                        SEXP vnum, SEXP split2,
+                        SEXP csplit2, SEXP usesur, SEXP xdata2, SEXP wt2, SEXP y2,
+                        SEXP xmiss2);
 static const R_CallMethodDef CallEntries[] = {
     {"init_rpcallback", (DL_FUNC) &init_rpcallback, 5},
     {"rpart", (DL_FUNC) &rpart, 11},
     {"xpred", (DL_FUNC) &xpred, 15},
     {"rpartexp2", (DL_FUNC) &rpartexp2, 2},
     {"pred_rpart", (DL_FUNC) &pred_rpart, 12},
+    {"honest_estimate_rpart", (DL_FUNC) &honest_estimate_rpart, 18},
     {NULL, NULL, 0}
 };
 
